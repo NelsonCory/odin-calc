96145d99d82eec3c401b986286caebc4e8049d7e
Cory Nelson <34819470+NelsonCory@users.noreply.github.com>
2024-06-11 00:43:23 -0500

    initialize calculator layout
    create initial js functions for major operations

---
+++ calculator.js
@@ -0,0 +1,30 @@
+//calculator
+
+
+function add(x,y){
+    return x + y;
+}
+
+function subtract(x,y){
+    return x - y;
+}
+
+function multiply(x,y){
+    return x * y;
+}
+
+function divide(x,y){
+    return x/y; 
+}
+
+function square(x){
+    return x * x;
+}
+
+function clear(){
+    console.log("DEBUG: CLEAR PRESSED");
+}
+
+function percent(x){
+    console.log("DEBUG: PERCENT PRESSED");
+}

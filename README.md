0e579953c46118bc24d3c46b2c1dbc2da4074194
Cory Nelson <34819470+NelsonCory@users.noreply.github.com>
2024-06-11 01:11:27 -0500

    Initial Button Styling

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # odin-calc
 Simple HTML5 Calculator
+
+Performing the Odin Project Exercise, Foundations Calculator
+
+
+https://www.theodinproject.com/lessons/foundations-calculator
--- calculator.js
+++ calculator.js
@@ -1,5 +1,29 @@
 //calculator
 
+var displayValue = []
+
+function drawDisplay(){
+
+
+
+    document.getElementById("numberDisplay").innerHTML = parseDisplay();
+}
+
+function parseDisplay(){
+    var numToPrint = "0";
+    if(displayValue.length == 0){
+        numToPrint = "0";
+    }
+    else{
+
+        //convert arr into string, to use as value
+
+        //Number.parseFloat()
+
+    }
+
+    return x;
+}
 
 function add(x,y){
     return x + y;
@@ -13,7 +37,7 @@ function multiply(x,y){
     return x * y;
 }
 
-function divide(x,y){
+function divide(x,y){ //edge case: dividing by zero
     return x/y; 
 }
 
@@ -23,8 +47,21 @@ function square(x){
 
 function clear(){
     console.log("DEBUG: CLEAR PRESSED");
+    //reset all values, set calculator's display to zero
 }
 
 function percent(x){
     console.log("DEBUG: PERCENT PRESSED");
+}
+
+function toggleNeg(){
+    //set first char in displayValue to - or remove front if front is -
+}
+
+function pushChar(x){
+    displayValue.push(x);
+}
+
+function deleteChar(x){
+
 }
